b4be74fd79a71ebd76eef76fe84ac1a5e1b53b45
samp101 <shmpos@gmail.com>
2021-10-18 16:48:22 +0300

    exercise #1

---
+++ Week-3/Day-2/Exercise-1-Java-Java.js
@@ -0,0 +1,53 @@
+// "534"
+//  
+
+
+
+
+
+
+// really the sheet will stop after 2 because its an error
+
+// 5 + "34" = 534 as a text with Conntation
+// 5 - "4" = error because 5 is an integer and 4 is a string
+// 10 % 5 = 2
+// 5 % 10 = .5
+// "Java" + "Script" = "JavaScript" because it connataion the text even the + is meant to be with and integer
+// " " + " " = "  "
+// " " + 0 = " 0"
+// true + true = 2 because true also has a set value as 1 so 1+1 = 2
+// true + false = 1 
+// false + true = 1
+// false - true = - 1
+// 3 - 4 = -1
+// "Bob" - "bill" = error the logic being since they arent integers it cant conntoniate it and will return an error 
+
+// really the sheet will stop after 2 because its an error
+
+// 5 + "34" = 534 as a text with Conntation
+// 5 - "4" = its really 1 because although 4 is a text it will conjugate it.
+// 10 % 5 = 2 this is really 0 because % is the remainder so 5 goes into 10 twice with 0 remainder
+// 5 % 10 = .5 the answer really is 5 not sure why
+// "Java" + "Script" = "JavaScript" because it connataion the text even the + is meant to be with and integer
+// " " + " " = "  "
+// " " + 0 = " 0"
+// true + true = 2 because true also has a set value as 1 so 1+1 = 2
+// true + false = 1 
+// false + true = 1
+// false - true = - 1
+// 3 - 4 = -1
+// "Bob" - "bill" = error the logic being since they arent integers it cant conntoniate it and will return an error 
+
+5 + "34"
+5 - "4"
+10 % 5
+5 % 10
+"Java" + "Script"
+" " + " "
+" " + 0
+true + true
+true + false
+false + true
+false - true
+3 - 4
+"Bob" - "bill"
