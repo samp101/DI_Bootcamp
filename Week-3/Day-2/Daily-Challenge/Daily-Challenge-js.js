9a03d0551c59c7ac0dadf6f313c9ae90145cb8f2
samp101 <shmpos@gmail.com>
2021-10-18 19:32:08 +0300

    daily challenge

---
+++ Week-3/Day-2/Daily-Challenge/Daily-Challenge-js.js
@@ -0,0 +1,37 @@
+
+
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+fruits.shift()
+
+console.log(fruits)
+
+let fruits1 = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+fruits1.sort()
+
+console.log(fruits1)
+
+let fruits2 = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+fruits2.push("kiwi")
+
+console.log(fruits2)
+
+let fruits3 = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+fruits3.splice(1,1,)
+
+console.log(fruits3)
+
+let fruits4 = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+fruits4.reverse()
+
+console.log(fruits4)
+
+// Exercise 2
+
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+console.log(moreFruits[1][1])
